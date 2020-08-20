c6ed1dc9bc5bbd0f802e64aa9e9d5e4120ae20db
Dylan Kreth <dylankreth@gmail.com>
2020-08-19 20:37:03 -0700

    fix: replace lombok with vanilla Java bc it was being wonky

--- src/main/java/com/dkreth/shorty/model/Record.java
+++ src/main/java/com/dkreth/shorty/model/Record.java
@@ -1,11 +1,19 @@
 package com.dkreth.shorty.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class Record {
     private String shortId;
     private String longUrl;
+
+    public Record(String shortId, String longUrl) {
+        this.shortId = shortId;
+        this.longUrl = longUrl;
+    }
+
+    public String getShortId() {
+        return shortId;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
 }
