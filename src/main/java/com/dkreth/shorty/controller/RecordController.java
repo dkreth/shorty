b4f5e875ecea8d048c1a5be2bc2e279fd3e696af
Dylan Kreth <dylankreth@gmail.com>
2020-08-19 21:07:32 -0700

    feat: Post endpoint for /generate and Get endpoint for /{shortId}

--- src/main/java/com/dkreth/shorty/controller/RecordController.java
+++ src/main/java/com/dkreth/shorty/controller/RecordController.java
@@ -1,14 +1,28 @@
 package com.dkreth.shorty.controller;
 
+import com.dkreth.shorty.data.RecordInMemoryRepository;
 import com.dkreth.shorty.model.Record;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class RecordController {
 
+    private final RecordInMemoryRepository recordRepository = new RecordInMemoryRepository();
+
     @GetMapping("test")
     public Record testPath() {
         return new Record("test", "test");
+    }
+
+    @PostMapping("/generate")
+    public Record generate(@RequestBody Record newRecord){
+        return recordRepository.saveRecord(newRecord);
+    }
+
+    @GetMapping("/{shortId}")
+    public RedirectView redirect(@PathVariable String shortId) {
+        Record record = recordRepository.getRecordByShortId(shortId);
+        return new RedirectView("http://" + record.getLongUrl());
     }
 }
