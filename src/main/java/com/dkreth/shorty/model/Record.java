a6f0e80bf8a0a97e0d581022dc283bb905bd10ef
Dylan Kreth <dylankreth@gmail.com>
2020-08-20 20:49:39 -0700

    feat: add id to Record model for use in MongoDB

--- src/main/java/com/dkreth/shorty/model/Record.java
+++ src/main/java/com/dkreth/shorty/model/Record.java
@@ -1,6 +1,13 @@
 package com.dkreth.shorty.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "shorty test data")
 public class Record {
+
+    @Id
+    private String id;
     private String shortId;
     private String longUrl;
 
