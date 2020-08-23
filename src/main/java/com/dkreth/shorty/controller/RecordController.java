d4e45083de2edcb47d6e674fd222ee5faf73d79a
Dylan Kreth <dylankreth@gmail.com>
2020-08-23 00:20:42 -0700

    feat: generate returns 409 on shortIdAlreadyExists and 201 on created

--- src/main/java/com/dkreth/shorty/controller/RecordController.java
+++ src/main/java/com/dkreth/shorty/controller/RecordController.java
@@ -1,9 +1,10 @@
 package com.dkreth.shorty.controller;
 
-import com.dkreth.shorty.data.RecordInMemoryRepository;
 import com.dkreth.shorty.data.RecordRepository;
 import com.dkreth.shorty.model.Record;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -22,9 +23,12 @@ public class RecordController {
     }
 
     @PostMapping("/generate")
-    public Record generate(@RequestBody Record newRecord){
-        //TODO - 422 if the provided shortId already exists
-        return recordRepository.insert(newRecord);
+    public ResponseEntity generate(@RequestBody Record newRecord){
+        Record foundRecord = recordRepository.findByShortId(newRecord.getShortId());
+        if(foundRecord != null) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity(recordRepository.insert(newRecord), HttpStatus.CREATED);
     }
 
     @GetMapping("/{shortId}")
